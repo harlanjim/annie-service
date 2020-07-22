a25c0e78276ae55dea11767cb49ef22aa2d1b727
jim <jim@jim-VGN-NW238F>
2020-07-22 17:06:45 -0500

    Initial creation.

---
+++ src/main/java/com/fah/enterprises/AnnieServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fah.enterprises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnieServiceTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnnieServiceTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fah/enterprises/AnnieServiceTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fah.enterprises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnieServiceTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
