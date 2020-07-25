fad78770eac38f8d96cd3e5748d42b36e395925d
jim <jim@jim-VGN-NW238F>
2020-07-25 13:37:55 -0500

    more clean up

---
+++ src/main/java/com/fah/enterprises/controllers/HomeResource.java
@@ -0,0 +1,23 @@
+package com.fah.enterprises.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeResource {
+
+	@GetMapping("/")
+	public String home() {
+		return "<h1>Hello</h1>";
+	}
+	
+	@GetMapping("/user")
+	public String user() {
+		return "<h1>Hello User</h1>";
+	}
+
+	@GetMapping("/admin")
+	public String admin() {
+		return "<h1>Hello Admin</h1>";
+	}
+}
