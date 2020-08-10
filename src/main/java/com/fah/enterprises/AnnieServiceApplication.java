44ac8fc103602a3183e21542941ae075955f9d14
chrx <chrx@chrx>
2020-08-10 01:32:55 +0000

    fix create table on startup..

--- src/main/java/com/fah/enterprises/AnnieServiceApplication.java
+++ src/main/java/com/fah/enterprises/AnnieServiceApplication.java
@@ -1,6 +1,7 @@
 package com.fah.enterprises;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -8,6 +9,7 @@ import com.fah.enterprises.repositories.UserRepository;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackageClasses = UserRepository.class)
+@EnableAutoConfiguration
 public class AnnieServiceApplication {
 
 	public static void main(String[] args) {
