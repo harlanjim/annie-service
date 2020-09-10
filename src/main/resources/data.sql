4480a0693419db85dff58acf407f2261efe67ee5
jim <jim@jim-VirtualBox>
2020-09-10 14:15:08 -0500

    Fix sequence name

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,1 @@
-insert into "public"."annieuser" (id, active, password, roles, user_name) values (nextval('myuser_id_seq'), true, 'pass', 'USER', 'jim');
+insert into "public"."annieuser" (id, active, password, roles, user_name) values (nextval('annieuser_id_seq'), true, 'pass', 'USER', 'jim');
