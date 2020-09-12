7ccec4fa8f38e78c48b09e36ad5e72c63c3d27bb
jim <jim@jim-VirtualBox>
2020-09-11 20:31:55 -0500

    API Security Working

--- src/main/java/com/fah/enterprises/controllers/HomeResource.java
+++ src/main/java/com/fah/enterprises/controllers/HomeResource.java
@@ -1,16 +1,38 @@
 package com.fah.enterprises.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fah.enterprises.models.AuthenticationRequest;
+import com.fah.enterprises.models.AuthenticationResponse;
+import com.fah.enterprises.utils.JwtUtil;
 
 @RestController
 public class HomeResource {
+
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
+	@Autowired
+	private JwtUtil jwtTokenUtil;
 
 	@GetMapping("/test")
 	public String home() {
 		return "<h1>Hello</h1>";
 	}
-	
+
 	@GetMapping("/user")
 	public String user() {
 		return "<h1>Hello User</h1>";
@@ -20,4 +42,23 @@ public class HomeResource {
 	public String admin() {
 		return "<h1>Hello Admin</h1>";
 	}
+
+	@PostMapping(value = "/authenticate")
+	public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest)
+			throws Exception {
+
+		try {
+			authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+					authenticationRequest.getUsername(), authenticationRequest.getPassword()));
+		} catch (BadCredentialsException e) {
+			throw new Exception("Incorrect username or password", e);
+		}
+
+		final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+
+		final String jwt = jwtTokenUtil.generateToken(userDetails);
+
+		return ResponseEntity.ok(new AuthenticationResponse(jwt));
+	}
+
 }
--- src/main/java/com/fah/enterprises/security/SecurityConfiguration.java
+++ src/main/java/com/fah/enterprises/security/SecurityConfiguration.java
@@ -2,19 +2,27 @@ package com.fah.enterprises.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.fah.enterprises.filter.JwtRequestFilter;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
 	UserDetailsService userDetailsService;
+	
+	@Autowired
+	private JwtRequestFilter jwtRequestFilter;
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -23,11 +31,29 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
+		http.csrf().disable()
+			.authorizeRequests()
 			.antMatchers("/admin/**").hasAnyRole("ADMIN")
 			.antMatchers("/user/**").hasAnyRole("USER", "ADMIN")
-			.antMatchers("/**").permitAll()
-			.and().formLogin();
+			.antMatchers("/authenticate").permitAll().
+			anyRequest().authenticated().and().
+			exceptionHandling().and().sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+//			.antMatchers("/admin/**").hasAnyRole("ADMIN")
+//			.antMatchers("/user/**").hasAnyRole("USER", "ADMIN")
+//			.antMatchers("/**").permitAll()
+//			.and().formLogin();
+		/*
+		 * 		httpSecurity.csrf().disable()
+				.authorizeRequests()
+		 */
+	}
+	
+	@Override
+	@Bean
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
 	}
 	
 	@Bean
