d6376218dbe57b99100607c65531656b130bdd90
fah.jim@gmail.com <fah.jim@gmail.com>
2020-11-21 20:06:14 +0000

    Allow local CORS

--- src/main/java/com/fah/enterprises/AnnieServiceApplication.java
+++ src/main/java/com/fah/enterprises/AnnieServiceApplication.java
@@ -3,7 +3,10 @@ package com.fah.enterprises;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.fah.enterprises.repositories.UserRepository;
 
@@ -16,4 +19,13 @@ public class AnnieServiceApplication {
 		SpringApplication.run(AnnieServiceApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/*").allowedOrigins("http://localhost:8080");
+			}
+		};
+	}
 }
